ad2755cb4255d1622566b43405b2cd1b480d5634
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-14 21:36:14 +0600

    first commit

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,17 +1,36 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app'
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId,
+// }
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig)
+
+
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-}
+  apiKey: "AIzaSyBQv9n_b7TfNhYIk1BideGmJ5KIe-1w2q8",
+  authDomain: "solophere-23.firebaseapp.com",
+  projectId: "solophere-23",
+  storageBucket: "solophere-23.firebasestorage.app",
+  messagingSenderId: "890995799242",
+  appId: "1:890995799242:web:2d14271c6d4f42c018f033"
+};
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
